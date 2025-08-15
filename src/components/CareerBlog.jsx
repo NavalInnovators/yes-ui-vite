b817b2e91b6bb7e95e56ee5c5bee72f1420edbdc
Raman <kataware.doki2005@gmail.com>
2025-08-15 14:15:02 +0530

    init commit

--- src/components/CareerBlog.jsx
+++ src/components/CareerBlog.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useMemo, useEffect } from "react";
+import { useState, useMemo, useEffect } from "react";
 import "./CareerBlog.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { jobData, jobCategories } from "../constants";
 import CTA from "./CTA";
 
 const CareerBlog = () => {
-  const [searchInput, setSearchInput] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [searchInput, setSearchInput] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [isMobile, setIsMobile] = useState(false);
   const navigate = useNavigate();
 
@@ -15,15 +15,15 @@ const CareerBlog = () => {
     const checkIfMobile = () => {
       setIsMobile(window.innerWidth <= 768);
     };
-    
+
     // Initial check
     checkIfMobile();
-    
+
     // Add event listener for window resize
-    window.addEventListener('resize', checkIfMobile);
-    
+    window.addEventListener("resize", checkIfMobile);
+
     // Clean up
-    return () => window.removeEventListener('resize', checkIfMobile);
+    return () => window.removeEventListener("resize", checkIfMobile);
   }, []);
 
   const handleViewJob = (job) => {
@@ -35,18 +35,18 @@ const CareerBlog = () => {
   };
 
   const filteredJobs = useMemo(() => {
-    return jobData.filter(job => {
-      const matchesSearch = 
+    return jobData.filter((job) => {
+      const matchesSearch =
         job.title.toLowerCase().includes(searchInput.toLowerCase()) ||
         job.description.toLowerCase().includes(searchInput.toLowerCase()) ||
-        job.role_and_responsibilities.some(resp => 
+        job.role_and_responsibilities.some((resp) =>
           resp.toLowerCase().includes(searchInput.toLowerCase())
         ) ||
-        job.qualifications_and_experience.some(qual => 
+        job.qualifications_and_experience.some((qual) =>
           qual.toLowerCase().includes(searchInput.toLowerCase())
         );
 
-      const matchesCategory = 
+      const matchesCategory =
         !selectedCategory || job.category === selectedCategory;
 
       return matchesSearch && matchesCategory;
@@ -56,12 +56,15 @@ const CareerBlog = () => {
   return (
     <div className="career-blog">
       {/* Header Section */}
-      <header className="header">
+      <div className="header">
         <div className="header-heading">Career</div>
         <div className="header-para">
-        Welcome to the Career page, your gateway to exciting opportunities that align with your aspirations. Explore diverse roles, apply your skills, and embark on a journey toward professional growth and success.
+          Welcome to the Career page, your gateway to exciting opportunities
+          that align with your aspirations. Explore diverse roles, apply your
+          skills, and embark on a journey toward professional growth and
+          success.
         </div>
-      </header>
+      </div>
 
       {/* Search Bar Section */}
       <div className="career-search-bar">
@@ -72,16 +75,22 @@ const CareerBlog = () => {
           onChange={(e) => setSearchInput(e.target.value)}
           className="input-box"
         />
-        
+
         {/* Custom CSS Dropdown replacing select element */}
         <div className="custom-dropdown">
           <div className="dropdown-button">
             {isMobile && !selectedCategory ? (
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
-                <path d="M440-160q-17 0-28.5-11.5T400-200v-240L168-736q-15-20-4.5-42t36.5-22h560q26 0 36.5 22t-4.5 42L560-440v240q0 17-11.5 28.5T520-160h-80Zm40-308 198-252H282l198 252Zm0 0Z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="currentColor"
+              >
+                <path d="M440-160q-17 0-28.5-11.5T400-200v-240L168-736q-15-20-4.5-42t36.5-22h560q26 0 36.5 22t-4.5 42L560-440v240q0 17-11.5 28.5T520-160h-80Zm40-308 198-252H282l198 252Zm0 0Z" />
               </svg>
             ) : (
-              <span>{selectedCategory || 'All Categories'}</span>
+              <span>{selectedCategory || "All Categories"}</span>
             )}
             <svg
               className="dropdown-arrow"
@@ -99,9 +108,9 @@ const CareerBlog = () => {
             </svg>
           </div>
           <div className="dropdown-content">
-            <div 
+            <div
               className="dropdown-item"
-              onClick={() => handleCategorySelect('')}
+              onClick={() => handleCategorySelect("")}
             >
               All Categories
             </div>
@@ -120,30 +129,61 @@ const CareerBlog = () => {
 
       {/* Job Card Section */}
       <div className="job-card-section">
-        {
-          filteredJobs.length === 0 ? (
-              <p>No jobs found</p>
-          ) : (filteredJobs.map((job, index) => (
+        {filteredJobs.length === 0 ? (
+          <p>No jobs found</p>
+        ) : (
+          filteredJobs.map((job, index) => (
             <div key={index} className="job-card">
               <div className="main-container">
-                <div className="career-title font-subheading-black">{job.title}</div>
-                <div className="career-description font-paragraph">{job.description}</div>
+                <div className="career-title font-subheading-black">
+                  {job.title}
+                </div>
+                <div className="career-description font-paragraph">
+                  {job.description}
+                </div>
               </div>
               <div className="card-buttons">
                 <button
-                    className="apply-btn"
-                    onClick={() => window.open(job.link, '_blank', 'noopener,noreferrer')}
+                  className="apply-btn"
+                  onClick={() =>
+                    window.open(job.link, "_blank", "noopener,noreferrer")
+                  }
                 >
-                    Apply
+                  Apply
                 </button>
-                <button className="view-btn" onClick={() => handleViewJob(job)}>View Job</button>
+                <button className="view-btn" onClick={() => handleViewJob(job)}>
+                  View Job
+                </button>
               </div>
             </div>
-          )))
-        }
+          ))
+        )}
       </div>
 
-      <CTA/>
+      {/* Creator Verification Section */}
+      <div className="my-20 mx-auto">
+        <div className="max-w-[942px] mx-auto py-16 border-t border-light-border">
+          <h1 className="text-5xl text-center">Become a Creator and Earn</h1>
+
+          <p className="mt-[30px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, et
+            sunt modi, incidunt cum recusandae velit tenetur enim reiciendis
+            atque officia obcaecati vero dolor fugiat harum eligendi blanditiis
+            veniam magnam ratione nam. Ducimus, delectus! Accusamus ab
+            blanditiis suscipit voluptates aperiam, temporibus excepturi
+            molestiae dolores culpa labore quidem. Quisquam perspiciatis
+            corporis earum consectetur laborum! Odit sint omnis tempore.
+            Tenetur, rerum magnam.
+          </p>
+        </div>
+
+        {/* Verification Process Div */}
+        <div className="">
+          <div className="header"></div>
+        </div>
+      </div>
+
+      <CTA />
     </div>
   );
 };
--- src/components/CareerJob.jsx
+++ src/components/CareerJob.jsx
@@ -6,50 +6,58 @@ function CareerJob() {
   const { job } = location.state || {}; // Access job data passed via navigation
 
   if (!job) {
-    return <div className="loading-container" style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      height: '100vh', 
-      fontSize: '1.2rem' 
-    }}>Loading job details...</div>;
+    return (
+      <div
+        className="loading-container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          fontSize: "1.2rem",
+        }}
+      >
+        Loading job details...
+      </div>
+    );
   }
 
   return (
     <div className="careerJob-page">
       {/* Career Header */}
       <div className="header font-header">
-        <div className="main-heading">
-          {job.title}
-        </div>
+        <div className="main-heading">{job.title}</div>
       </div>
 
       {/* Job Card */}
       <div className="career-jobcard">
         <div className="inner-jobcard">
           <div className="btn">
-            <button className="apply-btn font-black-btn" onClick={() => window.open(job.link, "_blank", "noopener,noreferrer")}>Apply</button>
+            <button
+              className="apply-btn font-black-btn"
+              onClick={() =>
+                window.open(job.link, "_blank", "noopener,noreferrer")
+              }
+            >
+              Apply
+            </button>
           </div>
 
           {/* Roles and Responsibilities */}
           <div className="section-carrer">
-            <div className="section--heading">
-              Roles and Responsibilities
-            </div>
+            <div className="section--heading">Roles and Responsibilities</div>
             <ul>
-            {job.role_and_responsibilities.map((role, index) => (
+              {job.role_and_responsibilities.map((role, index) => (
                 <li key={index}>{role}</li>
               ))}
             </ul>
           </div>
-          
+
           {/* Qualifications and Experience */}
           <div className="section-carrer">
-            <div className="section--heading">
-              Qualifications & Experience
-            </div>
+            <div className="section--heading">Qualifications & Experience</div>
             <ul>
-            {job.qualifications_and_experience.map((qual, index) => (
+              {job.qualifications_and_experience.map((qual, index) => (
                 <li key={index}>{qual}</li>
               ))}
             </ul>
