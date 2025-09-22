97263b620df744f58ca6d98ba0a8abd2e2ee3e8b
Sachin Sharma <sachinnaval5@gmail.com>
2025-09-23 00:50:41 +0530

    Removed unused files

--- src/roles/Admin/pages/draft_page/components/DraftHoverInfo.jsx
+++
@@ -1,7 +0,0 @@
-export default function DraftHoverInfo({ message }) {
-  return (
-    <div className="absolute top-[45px] right-[-20px] info-tooltip w-[290px] py-[5px] px-[10px] shadow-[0px_0px_3px_1px_rgba(0,_0,_0,_0.1)] rounded-[7px]">
-      {message}
-    </div>
-  );
-}
--- src/roles/Admin/pages/draft_page/context/CommentTextContext.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-const CommentTextContext = createContext();
-export default CommentTextContext;
