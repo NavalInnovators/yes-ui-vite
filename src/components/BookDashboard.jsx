b60e1fa8bf232f4c0c9a300d578cbef2eb77d41a
Sachin Sharma <sachinnaval5@gmail.com>
2025-09-22 14:27:29 +0530

    Rename Popup File

--- src/components/BookDashboard.jsx
+++ src/components/BookDashboard.jsx
@@ -11,7 +11,7 @@ import BookDashboardSyllabus from "./BookDashboardSyllabus";
 // import BookDashboardMap from "./BookDashboardMap";
 import BookDashboardMidSec from "./BookDashboardMidSec";
 import BookDashboardUnitEmptyRightSec from "./BookDashboardUnitEmptyRightSec";
-import PlanPopup from "../components/PlanPopUp.jsx";
+import PlanPopup from "../components";
 
 const BookDashboardSections = [
   {
